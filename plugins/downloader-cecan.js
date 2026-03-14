6ba3599feae599630f693edb5f9c760e5f9a5cd0
paul christi <rachrisgamer@gmail.com>
2026-03-14 15:47:44 +0700

    ADDED FEATUREv2

--- plugins/downloader-cecan.js
+++ plugins/downloader-cecan.js
@@ -1,15 +1,8 @@
 let fetch = require('node-fetch')
 
 let handler = async (m, { conn, command }) => {
-    let api1 = `https://api.betabotz.eu.org/api/cecan/${command}?apikey=${lann}`
-    let api2 = `https://api.botcahx.eu.org/api/cecan/${command}?apikey=${btc}`
-    let buffer = await fetch(api1)
-        .then(res => res.buffer())
-        .catch(async (err) => {
-            console.log(`API 1 failed with error: ${err}. Trying API 2...`)
-            buffer = await fetch(api2).then(res => res.buffer())
-            return buffer
-        })
+    let api = `https://api.betabotz.eu.org/api/cecan/${command}?apikey=${lann}`
+    let buffer = await fetch(api).then(res => res.buffer())
     conn.sendFile(m.chat, buffer, 'hasil.jpg', `Random ${command}`, m)
 }
 
